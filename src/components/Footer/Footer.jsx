6e7ef4f08810b237f2c179c5149d15779358b9df
Ryne Carter <carterryne@gmail.com>
2023-07-10 19:09:47 -0500

    homepage shows characters

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <button className='contact-button'>Contact Us</button>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react'
+import './Homepage.css'
+import axios from 'axios'
+import CharacterCard from '../../components/CharacterCard/CharacterCard'
+
+function Homepage() {
+  //create state for the characters
+  const [characters, setCharacters] = useState([])
+  //this page shows characters when it loads
+  //https://rickandmortyapi.com/api/character
+
+  useEffect(
+    ()=>{
+      console.log('homepage loaded')
+      //make api call to get character data
+      axios.get(`https://rickandmortyapi.com/api/character`)
+      .then(res =>{
+        console.log(res.data.results)
+        //I have the data, where do I store it?
+        //store in state
+        setCharacters(res.data.results)
+        
+      })
+      .catch(err => console.log(err))
+
+    }, [] //run once when the page loads
+  )
+
+  return (
+    <div className='homepage-container'>
+      <h1>Main Characters</h1>
+      <div className='characters-container'>
+        {
+          characters.map(item => <CharacterCard key={item.id} character={item}/>)
+
+          //characters.map(item => <p key={item.id}>{item.name}</p>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
